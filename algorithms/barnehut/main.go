3172210652f221f3b8a76e25789b57db81e6f790
Ivan Danyliuk <ivan.daniluk@gmail.com>
2017-11-30 21:37:01 +0100

    Add webonly flag

--- algorithms/barnehut/main.go
+++ algorithms/barnehut/main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 )
 
 func main() {
-	data, err := NewGraphDataFromJSON("static/data.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Loaded graph: %d nodes, %d links\n", len(data.Nodes), len(data.Links))
+	var webOnly = flag.Bool("web", false, "Don't generate data, just serve web page")
+	flag.Parse()
 
-	layout := &Layout3D{}
-	layout.InitCoordinates(data.Nodes)
+	if !*webOnly {
+		data, err := NewGraphDataFromJSON("static/data.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Loaded graph: %d nodes, %d links\n", len(data.Nodes), len(data.Links))
 
-	out := NewNgraphBinaryOutput("./static/data")
-	err = out.Save(layout, data)
-	if err != nil {
-		log.Fatal(err)
+		layout := &Layout3D{}
+		layout.InitCoordinates(data.Nodes)
+
+		out := NewNgraphBinaryOutput("./static/data")
+		err = out.Save(layout, data)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	err = startWeb()
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(startWeb())
 }
