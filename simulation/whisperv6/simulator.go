ff73374cacf4b16c444249695be2261e1041f6f9
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-04-11 18:22:50 +0200

    Refactor whisperv6 simulator

--- simulation/whisperv6/simulator.go
+++ simulation/whisperv6/simulator.go
@@ -1,8 +1,8 @@
 package whisperv6
 
 import (
+	"fmt"
 	"log"
-	"time"
 
 	"github.com/divan/graph-experiments/graph"
 	"github.com/divan/graph-experiments/simulation"
@@ -16,18 +16,13 @@ import (
 // Simulator simulates WhisperV6 message propagation through the
 // given p2p network.
 type Simulator struct {
-	data *graph.Data
+	data    *graph.Data
+	network *simulations.Network
+	nodes   []*simulations.Node
 }
 
 // NewSimulator intializes simulator for the given graph data.
 func NewSimulator(data *graph.Data) *Simulator {
-	return &Simulator{
-		data: data,
-	}
-}
-
-// SendMessage sends single message and tracks propagation. Implements simulator.Interface.
-func (s *Simulator) SendMessage(startNodeIdx, ttl int) *simulation.Log {
 	services := map[string]adapters.ServiceFunc{
 		"ping-pong": func(ctx *adapters.ServiceContext) (node.Service, error) {
 			return newPingPongService(ctx.Config.ID), nil
@@ -35,45 +30,68 @@ func (s *Simulator) SendMessage(startNodeIdx, ttl int) *simulation.Log {
 	}
 	adapters.RegisterServices(services)
 
-	var adapter adapters.NodeAdapter
-	adapter = adapters.NewSimAdapter(services)
-
+	adapter := adapters.NewSimAdapter(services)
 	network := simulations.NewNetwork(adapter, &simulations.NetworkConfig{
 		DefaultService: "ping-pong",
 	})
 
-	log.Println("Creating nodes...")
-	node1, err := network.NewNodeWithConfig(nodeConfig("Node 1"))
-	if err != nil {
-		panic(err)
+	nodeCount := len(data.Nodes)
+	sim := &Simulator{
+		data:    data,
+		network: network,
+		nodes:   make([]*simulations.Node, nodeCount),
 	}
-	node2, err := network.NewNodeWithConfig(nodeConfig("Node 2"))
-	if err != nil {
-		panic(err)
+
+	log.Println("Creating nodes...")
+	for i := 0; i < nodeCount; i++ {
+		node, err := sim.network.NewNodeWithConfig(nodeConfig(i))
+		if err != nil {
+			panic(err)
+		}
+		sim.nodes[i] = node
 	}
 
 	log.Println("Starting nodes...")
-	if err := network.Start(node1.ID()); err != nil {
-		panic(err)
+	for i := 0; i < nodeCount; i++ {
+		err := network.Start(sim.nodes[i].ID())
+		if err != nil {
+			panic(err)
+		}
 	}
-	if err := network.Start(node2.ID()); err != nil {
-		panic(err)
-	}
-	defer network.StopAll()
 
 	log.Println("Connecting nodes...")
-	if err := network.Connect(node1.ID(), node2.ID()); err != nil {
-		panic(err)
+	for _, link := range data.Links {
+		fromIdx, err := findNode(data.Nodes, link.Source)
+		if err != nil {
+			panic(err)
+		}
+		toIdx, err := findNode(data.Nodes, link.Target)
+		if err != nil {
+			panic(err)
+		}
+		node1 := sim.nodes[fromIdx]
+		node2 := sim.nodes[toIdx]
+		if err := network.Connect(node1.ID(), node2.ID()); err != nil {
+			panic(err)
+		}
 	}
 
-	log.Println("Sleeping 10 secs...")
-	time.Sleep(10 * time.Second)
+	return sim
+}
 
+// Stop stops simulator and frees all resources if any.
+func (s *Simulator) Stop() error {
+	log.Println("Shutting down simulation nodes...")
+	return s.network.StopAll()
+}
+
+// SendMessage sends single message and tracks propagation. Implements simulator.Interface.
+func (s *Simulator) SendMessage(startNodeIdx, ttl int) *simulation.Log {
 	return &simulation.Log{}
 }
 
 // nodeConfig generates config for simulated node with random key.
-func nodeConfig(name string) *adapters.NodeConfig {
+func nodeConfig(idx int) *adapters.NodeConfig {
 	key, err := crypto.GenerateKey()
 	if err != nil {
 		panic("unable to generate key")
@@ -84,6 +102,21 @@ func nodeConfig(name string) *adapters.NodeConfig {
 	return &adapters.NodeConfig{
 		ID:         id,
 		PrivateKey: key,
-		Name:       name,
+		Name:       nodeIdxToName(idx),
 	}
+}
+
+func nodeIdxToName(id int) string {
+	return fmt.Sprintf("Node %d", id)
+}
+
+// findNode is a helper for finding node index by it's ID.
+// TODO: remove this when links replaces into indexes.
+func findNode(nodes []*graph.Node, ID string) (int, error) {
+	for i := range nodes {
+		if nodes[i].ID == ID {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("Node with ID '%s' not found", ID)
 }
